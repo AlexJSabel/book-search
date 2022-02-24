7c78eb512980254c85b1b0133872b29992f9e68e
alexjsabel <alexj.sabel@gmail.com>
2022-02-23 18:35:58 -0600

    fixing deployment issues

---
+++ client/build/precache-manifest.26dfa90b701f397f6b1629d3fd268096.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533ad8d498a5da536e4b825b0acee987",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e32a9d9d48f1f33877e2",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "27e492f0e6f0b903b5e4",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "e32a9d9d48f1f33877e2",
+    "url": "/static/js/2.ceb931a5.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.ceb931a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27e492f0e6f0b903b5e4",
+    "url": "/static/js/main.4a8664c5.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,52 @@
+// import gql
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    
+    type Book {
+        bookId: ID!
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Query {
+        me: User
+        users: [User]
+        user(username: String!): User
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    input AddNewBook {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(addBook: AddNewBook!): User
+        removeBook(removeBook: ID!): User
+    }
+
+`
+
+module.exports = typeDefs;
